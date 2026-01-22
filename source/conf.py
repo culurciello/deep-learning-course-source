d2e51f7ebc5fe5ba65376fdbef368cbe7560a166
Eugenio Culurciello <culurciello@gmail.com>
2026-01-22 13:03:36 -0500

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -15,7 +15,8 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "myst_nb",
+    # "myst_nb",
+    # 'sphinx.ext.mathjax'
     # "myst_parser",
 ]
 
